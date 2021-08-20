92c1f9436d14a4554e6c085c5bda02f4a369b18d
Dennis B <bluz71@users.noreply.github.com>
2021-08-20 16:16:25 +1000

    Changed color palette script

--- misc/color-palette.sh
+++ misc/color-palette.sh
@@ -1,1 +1,1 @@
-convert -size 100x200 xc:"#ff5454" xc:"#e2637f" xc:"#ff5189" xc:"#d183e8" xc:"#ae81ff" xc:"#80a0ff" xc:"#74b2ff" xc:"#79dac8" xc:"#36c692" xc:"#8cc85f" xc:"#85dc85" xc:"#bfbf97" xc:"#e3c78a" xc:"#f09479" xc:"#de935f" +append moonfly-palette.png
+convert -size 100x200 xc:"#ff5454" xc:"#8cc85f" xc:"#e3c78a" xc:"#80a0ff" xc:"#d183e8" xc:"#79dac8" xc:"#e2637f" xc:"#36c692" xc:"#bfbf97" xc:"#74b2ff" xc:"#ae81ff" xc:"#f09479" +append moonfly-palette.png
