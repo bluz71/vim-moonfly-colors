bd51183ad84175f023261dbbef06870a5fe18001
Dennis B <bluz71@users.noreply.github.com>
2025-09-28 20:29:46 +1000

    feat: add styling for new ComplHint and ComplHintMore groups

--- lua/moonfly/init.lua
+++ lua/moonfly/init.lua
@@ -345,6 +345,8 @@ M.style = function()
   highlight(0, "Delimiter", { link = "MoonflyWhite" })
   highlight(0, "qfFileName", { link = "MoonflyEmerald" })
   highlight(0, "qfLineNr", { link = "MoonflyGrey39" })
+  highlight(0, "ComplHint", { link = "MoonflyGrey39" })
+  highlight(0, "ComplHintMore", { link = "MoonflyLavender" })
 
   -- Color column (after line 80)
   highlight(0, "ColorColumn", { bg = grey7 })
